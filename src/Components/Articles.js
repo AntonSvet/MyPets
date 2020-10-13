67fc3e2a7c54c35c83c22ab8d4bba1bd5bc2ef56
Anton <waytofall@inbox.ru>
2020-10-13 21:04:31 +0300

    first commit

---
+++ src/Components/Articles.js
@@ -0,0 +1,180 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div>
+            <div className="row mb-2">
+                <div className="col-md-6">
+                    <div
+                        className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div className="col p-4 d-flex flex-column position-static">
+                            <strong className="d-inline-block mb-2 text-primary">Кошки</strong>
+                            <h3 className="mb-0">Бенгальский котенок</h3>
+                            <div className="mb-1 text-muted">Окт 7</div>
+                            <p className="card-text mb-auto">Бенгальский котенок — является представителем гибридной породы кошек. Почему гибридной?</p>
+                            <a href="#" className="stretched-link">Продолжить чтение</a>
+                        </div>
+                        <div className="col-auto d-none d-lg-block">
+                            <img  src='https://drbengali.hu/files/d/a/da04cc9725c4faf7d8b3dfeddaa8cc22_300x300_c.png'/>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div
+                        className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div className="col p-4 d-flex flex-column position-static">
+                            <strong className="d-inline-block mb-2 text-success">Собаки</strong>
+                            <h3 className="mb-0">Цвергпинчер</h3>
+                            <div className="mb-1 text-muted">Окт 7</div>
+                            <p className="mb-auto">Цвергпинчер – миниатюрный, но весьма крепко сложенный красавец.</p>
+                            <a href="#" className="stretched-link">Продолжить чтение</a>
+                        </div>
+                        <div className="col-auto d-none d-lg-block">
+                            <img  src='https://zooshef.ru/image/catalog/images/store/trixie/shirley2.jpg'/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row mb-2">
+                <div className="col-md-6">
+                    <div
+                        className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div className="col p-4 d-flex flex-column position-static">
+                            <strong className="d-inline-block mb-2 text-primary">World</strong>
+                            <h3 className="mb-0">Featured post</h3>
+                            <div className="mb-1 text-muted">Nov 12</div>
+                            <p className="card-text mb-auto">This is a wider card with supporting text below as a
+                                natural lead-in to additional content.</p>
+                            <a href="#" className="stretched-link">Continue reading</a>
+                        </div>
+                        <div className="col-auto d-none d-lg-block">
+                            <svg className="bd-placeholder-img" width="200" height="250"
+                                 xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
+                                 focusable="false" role="img" aria-label="Placeholder: Thumbnail">
+                                <title>Placeholder</title>
+                                <rect width="100%" height="100%" fill="#55595c"></rect>
+                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div
+                        className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div className="col p-4 d-flex flex-column position-static">
+                            <strong className="d-inline-block mb-2 text-success">Design</strong>
+                            <h3 className="mb-0">Post title</h3>
+                            <div className="mb-1 text-muted">Nov 11</div>
+                            <p className="mb-auto">This is a wider card with supporting text below as a natural
+                                lead-in to additional content.</p>
+                            <a href="#" className="stretched-link">Continue reading</a>
+                        </div>
+                        <div className="col-auto d-none d-lg-block">
+                            <svg className="bd-placeholder-img" width="200" height="250"
+                                 xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
+                                 focusable="false" role="img" aria-label="Placeholder: Thumbnail">
+                                <title>Placeholder</title>
+                                <rect width="100%" height="100%" fill="#55595c"></rect>
+                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row mb-2">
+                <div className="col-md-6">
+                    <div
+                        className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div className="col p-4 d-flex flex-column position-static">
+                            <strong className="d-inline-block mb-2 text-primary">World</strong>
+                            <h3 className="mb-0">Featured post</h3>
+                            <div className="mb-1 text-muted">Nov 12</div>
+                            <p className="card-text mb-auto">This is a wider card with supporting text below as a
+                                natural lead-in to additional content.</p>
+                            <a href="#" className="stretched-link">Continue reading</a>
+                        </div>
+                        <div className="col-auto d-none d-lg-block">
+                            <svg className="bd-placeholder-img" width="200" height="250"
+                                 xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
+                                 focusable="false" role="img" aria-label="Placeholder: Thumbnail">
+                                <title>Placeholder</title>
+                                <rect width="100%" height="100%" fill="#55595c"></rect>
+                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div
+                        className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div className="col p-4 d-flex flex-column position-static">
+                            <strong className="d-inline-block mb-2 text-success">Design</strong>
+                            <h3 className="mb-0">Post title</h3>
+                            <div className="mb-1 text-muted">Nov 11</div>
+                            <p className="mb-auto">This is a wider card with supporting text below as a natural
+                                lead-in to additional content.</p>
+                            <a href="#" className="stretched-link">Continue reading</a>
+                        </div>
+                        <div className="col-auto d-none d-lg-block">
+                            <svg className="bd-placeholder-img" width="200" height="250"
+                                 xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
+                                 focusable="false" role="img" aria-label="Placeholder: Thumbnail">
+                                <title>Placeholder</title>
+                                <rect width="100%" height="100%" fill="#55595c"></rect>
+                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row mb-2">
+                <div className="col-md-6">
+                    <div
+                        className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div className="col p-4 d-flex flex-column position-static">
+                            <strong className="d-inline-block mb-2 text-primary">World</strong>
+                            <h3 className="mb-0">Featured post</h3>
+                            <div className="mb-1 text-muted">Nov 12</div>
+                            <p className="card-text mb-auto">This is a wider card with supporting text below as a
+                                natural lead-in to additional content.</p>
+                            <a href="#" className="stretched-link">Continue reading</a>
+                        </div>
+                        <div className="col-auto d-none d-lg-block">
+                            <svg className="bd-placeholder-img" width="200" height="250"
+                                 xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
+                                 focusable="false" role="img" aria-label="Placeholder: Thumbnail">
+                                <title>Placeholder</title>
+                                <rect width="100%" height="100%" fill="#55595c"></rect>
+                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div
+                        className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div className="col p-4 d-flex flex-column position-static">
+                            <strong className="d-inline-block mb-2 text-success">Design</strong>
+                            <h3 className="mb-0">Post title</h3>
+                            <div className="mb-1 text-muted">Nov 11</div>
+                            <p className="mb-auto">This is a wider card with supporting text below as a natural
+                                lead-in to additional content.</p>
+                            <a href="#" className="stretched-link">Continue reading</a>
+                        </div>
+                        <div className="col-auto d-none d-lg-block">
+                            <svg className="bd-placeholder-img" width="200" height="250"
+                                 xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
+                                 focusable="false" role="img" aria-label="Placeholder: Thumbnail">
+                                <title>Placeholder</title>
+                                <rect width="100%" height="100%" fill="#55595c"></rect>
+                                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Main
---
+++ src/Components/Content.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Context = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>HTML</li>
+            </ul>
+
+        </div>
+    )
+
+}
+export default Context
